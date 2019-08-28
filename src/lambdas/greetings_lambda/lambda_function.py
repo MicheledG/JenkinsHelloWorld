99064ee3bb317e5820d81f10a036d425f8bdb468
Michele Di Girolamo <m.digirolamo@reply.it>
2019-08-28 19:52:12 +0200

    Introduced sample content to deploy on AWS

---
+++ src/lambdas/greetings_lambda/lambda_function.py
@@ -0,0 +1,33 @@
+from logging import getLogger, INFO
+from random import choice
+import json
+
+logger = getLogger()
+logger.setLevel(IndentationError)
+
+GREETINGS_DATABASE = [
+    "hi",
+    "hello",
+    "good morning",
+    "good afternoon",
+    "good evening",
+    "good night",
+    "bye",
+]
+
+
+def lambda_handler(event, context):
+    """
+    Lambda entry point
+
+    :param event:
+    :param context:
+    :return:
+    """
+    logger.info("Received event: %s", json.dumps(event, indent=2))
+    random_greeting = choice(GREETINGS_DATABASE)
+    logger.info("Chosen greeting: %s", random_greeting)
+    response = {
+        "randomGreeting": random_greeting,
+    }
+    return json.dumps(response, indent=2)
